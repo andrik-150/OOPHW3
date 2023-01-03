afc3eaa66105434c1733bee72dfbc40f6105de47
Андрей <andrik150@rambler.ru>
2023-01-03 17:12:19 +0300

    Task

---
+++ VehicleDoor.java
@@ -0,0 +1,13 @@
+package HW3;
+
+public class VehicleDoor extends Door {
+
+    private DoorType doorType;
+
+    // создаем две двери авто, которые по умолчанию закрыты
+    public VehicleDoor(String name) {
+        this.doorType = DoorType.VEHICLE;
+        setState(State.CLOSED);
+        setName(name);
+    }
+}
